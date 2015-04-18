13b2aa4a43b08cb9a15892c59c04eca1fa0d5930
AbiTyasTunggal <abi@tyastunggal.com>
2015-04-18 16:08:02 +1000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -144,6 +144,18 @@
 
 - (void)update:(CCTime)delta
 {
+    // loop the ground
+    for (CCNode *ground in _grounds) {
+        // get the world position of the ground
+        CGPoint groundWorldPosition = [physicsNode convertToWorldSpace:ground.position];
+        // get the screen position of the ground
+        CGPoint groundScreenPosition = [self convertToNodeSpace:groundWorldPosition];
+        
+        // if the left corner is one complete width off the screen, move it to the right
+        if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
+            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
+        }
+    }
     // move and loop the bushes
     for (CCNode *bush in _bushes) {
         // move the bush
@@ -171,6 +183,7 @@
                                  2 * cloud.contentSize.width, cloud.position.y);
         }
     }
+
     _sinceTouch += delta;
     
     character.rotation = clampf(character.rotation, -30.f, 90.f);
@@ -185,7 +198,8 @@
     }
     
     physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
-       NSMutableArray *offScreenObstacles = nil;
+    
+    NSMutableArray *offScreenObstacles = nil;
     
     for (CCNode *obstacle in _obstacles) {
         CGPoint obstacleWorldPosition = [physicsNode convertToWorldSpace:obstacle.position];
